cb31ad9d23b80031a638ed22fa7118d4aab37504
msadityakumar <abhishek@abhisheks-MacBook-Pro.local>
2016-01-28 23:19:49 +0530

    first commit

---
+++ Assets/Ball.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	public AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		Rigidbody rigidBody = this.GetComponent<Rigidbody> ();
+		rigidBody.velocity = new Vector3 (0,0,500);
+		audioSource = this.GetComponent<AudioSource> ();
+		audioSource.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+
+	public Ball ball;
+	//Vector3 CameraOffset;
+	// Use this for initialization
+	void Start () {
+		//CameraOffset = new Vector3 (0,25,30);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	//	float zOffset = ball.transform.position.z
+		if(this.transform.position.z < 1829.0f)
+		{
+			this.transform.position = new Vector3 (0,50,ball.transform.position.z-100);
+
+		}
+	}
+}
